d8acee64013acfd6f2a398a0a89ade451218232a
oleksandr.roshchupki <oleksandr.roshchupkin@io-tahoe.com>
2019-03-28 20:28:56 +0200

    example of params path to servlet

--- proff4_examples_web/src/main/java/ua/kiev/prog/Serv.java
+++ proff4_examples_web/src/main/java/ua/kiev/prog/Serv.java
@@ -1,5 +1,6 @@
 package ua.kiev.prog;
 
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -11,6 +12,15 @@ public class Serv extends HttpServlet {
     @Override
     public void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws IOException {
+        processParams(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        processParams(req, resp);
+    }
+
+    private void processParams(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         String param1 = req.getParameter("param1");
         String param2 = req.getParameter("param2");
         resp.setContentType("text/html");
--- proff4_examples_web/src/main/webapp/index.jsp
+++ proff4_examples_web/src/main/webapp/index.jsp
@@ -1,5 +1,11 @@
 <html>
-<body>
-<h2>Hello World!</h2>
-</body>
+    <body>
+        <h2>Hello World!</h2>
+
+        <form action="/serv" method="post">
+            Name: <input type="text" name="param1" value="Zelensky"/> <br/>
+            Age: <input type="text" name="param2" value="43"/> <br/>
+            <input type="submit" value="send"/>
+        </form>
+    </body>
 </html>
