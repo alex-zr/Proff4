11577c08ad5fbf4c38d4f5400b56567f926b796f
oleksandr.roshchupki <oleksandr.roshchupkin@io-tahoe.com>
2019-05-07 18:20:30 +0300

    added security module

---
+++ proff4_examples_security/src/main/java/ua/kiev/prog/security/UserDetailsServiceImpl.java
@@ -0,0 +1,36 @@
+package ua.kiev.prog.security;
+
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@AllArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private UserService userService;
+    private PasswordEncoder passwordEncoder;
+
+    @Override
+    public UserDetails loadUserByUsername(String login) throws UsernameNotFoundException {
+        CustomUser customUser = userService.getUserByLogin(login);
+        if (customUser == null) {
+            throw new UsernameNotFoundException(login + " not found");
+        }
+
+        Set<GrantedAuthority> roles = new HashSet<>();
+        roles.add(new SimpleGrantedAuthority(customUser.getRole().toString()));
+
+        return new User(customUser.getLogin(), customUser.getPassword(), roles);
+    }
+}
